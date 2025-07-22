4f6692e1795d5e55d3ab7be2c1d5afcb8bcbc03d
lmschw <lillyschwarzenbach@gmail.com>
2025-07-22 10:41:29 +0200

    add implementation type to vacf simulation evaluation

--- vacf_eval.py
+++ vacf_eval.py
@@ -10,39 +10,42 @@ num_agents_options = [1, 5, 7, 10]
 steps_per_run = 25000
 num_runs = 30
 dimension = "2D"
+implementation_types = ["", "rtm_"]
 
 basepath = "J:/leader_emergence/results/2D/res/"
 
 for num_agents in num_agents_options:
     for sample_type in sample_types:
         for leader_type in leader_types:
-            results = []
-            for run in range(num_runs):
-                filename = f"{sample_type}_{leader_type}_{num_agents**2}_run{run + 1}"
-                filepath = f"{basepath}{filename}.pickle"
-                with open(filepath, "rb") as input_file:
-                    trajectory = pickle.load(input_file)["trajectory"]
+            for implementation_type in implementation_types:
+                results = []
+                for run in range(num_runs):
+                    filename = f"{implementation_type}{sample_type}_{leader_type}_{num_agents**2}_run{run + 1}"
+                    filepath = f"{basepath}{filename}.pickle"
+                    with open(filepath, "rb") as input_file:
+                        trajectory = pickle.load(input_file)["trajectory"]
 
-                    result = vacf.velocity_autocorrelation(trajectory)
-                    results.append(result)
+                        result = vacf.velocity_autocorrelation(trajectory)
+                        results.append(result)
 
-                    # Plot the VACF
+                        # Plot the VACF
+                        plt.figure(figsize=(8, 6))
+                        plt.plot(np.arange(1, len(result) + 1), result)
+                        plt.xlabel('Time Lag (τ)', fontsize=14)
+                        plt.ylabel('VACF (C_v(τ))', fontsize=14)
+                        plt.title(f'Velocity Autocorrelation Function (VACF) - {num_agents} agents, {sample_type}, {leader_type}, run={run}', fontsize=16)
+                        plt.grid(True)
+                        plt.savefig(f"{filename}.svg")
+                        plt.savefig(f"{filename}.png")
+                        plt.savefig(f"{filename}.pdf")
+                    filename = f"{implementation_type}{sample_type}_{leader_type}_{num_agents**2}"
+                    results = np.array(results)
                     plt.figure(figsize=(8, 6))
-                    plt.plot(np.arange(1, len(result) + 1), result)
+                    plt.plot(np.arange(1, len(results[0]) + 1), np.average(results.T, axis=1))
                     plt.xlabel('Time Lag (τ)', fontsize=14)
                     plt.ylabel('VACF (C_v(τ))', fontsize=14)
-                    plt.title(f'Velocity Autocorrelation Function (VACF) - {num_agents} agents, {sample_type}, {leader_type}, run={run}', fontsize=16)
+                    plt.title(f'Velocity Autocorrelation Function (VACF) - {num_agents} agents, {sample_type}, {leader_type}', fontsize=16)
                     plt.grid(True)
                     plt.savefig(f"{filename}.svg")
                     plt.savefig(f"{filename}.png")
                     plt.savefig(f"{filename}.pdf")
-                results = np.array(results)
-                plt.figure(figsize=(8, 6))
-                plt.plot(np.arange(1, len(results[0]) + 1), np.average(results.T, axis=1))
-                plt.xlabel('Time Lag (τ)', fontsize=14)
-                plt.ylabel('VACF (C_v(τ))', fontsize=14)
-                plt.title(f'Velocity Autocorrelation Function (VACF) - {num_agents} agents, {sample_type}, {leader_type}', fontsize=16)
-                plt.grid(True)
-                plt.savefig(f"{filename}.svg")
-                plt.savefig(f"{filename}.png")
-                plt.savefig(f"{filename}.pdf")
